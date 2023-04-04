e00086f150ebe2b1993808148717da5c93ee1672
Александр <alexandr@TECH.PARK>
2023-04-04 16:46:02 +0300

    Добавил поиск записок

---
+++ controller.py
@@ -0,0 +1,68 @@
+import json
+from pathlib import Path
+import view
+
+
+def Create_note(name, text, create):
+    db = Read_db()
+    id = len(db)+1
+    note = f"{id}; {name}; {text}; {create}"
+
+    with open("notebooks.db", "a", encoding='utf-8') as file:
+        file.write(f'\n{note}')
+        file.close()
+    print("\n"*100)
+    print("Запись сохранена\n")
+    view.Second_screen()
+
+def Read_db():
+    notes = []
+    with open("notebooks.db", "r", encoding='utf-8') as file:
+        temp = []
+        while True:
+            temp_note = {}
+            val = []
+            line = (file.readline().replace("\n", ""))
+            if line != "":
+                temp= line.split(";")
+            else:
+                break
+            val.append(temp[1])
+            val.append(temp[2])
+            val.append(temp[3])
+            temp_note[temp[0]] = val
+            notes.append(temp_note)
+        file.close()
+    return notes
+
+def Find_numb(numb):
+    db = Read_db()
+    result = {}
+    for item in db:
+        for key, val in item.items():
+            if numb == key:
+                result = item
+    return result
+
+def Find_name(name):
+    db = Read_db()
+    result = {}
+    for item in db:
+        for key, val in item.items():
+            if name in val[0]:
+                result = item
+    return result
+
+
+def Find_text(text):
+    db = Read_db()
+    result = {}
+    for item in db:
+        for key, val in item.items():
+            if text in val[1]:
+                result = item
+    return result
+
+def Del_note(numb):
+    db = Read_db()
+    print(db)
--- view.py
+++ view.py
@@ -1,20 +1,140 @@
+import datetime
+import controller
+
+
+
 def First_screen():
+    action = ''
     action = input(
         "Добро пожаловать в приложение Записки!\nВыберете действие:\n1 - Создать записку\n2 - Найти записку"
-        "\n3 - Просмотреть все записки\n4 - Редактировать записку\n5 - Удалить записку\n6 - Выход\nВведите "
+        "\n3 - Просмотреть все записки\n4 - Редактировать записку\n5 - Удалить записку\n6 - Выход из программы\nВведите "
         "цифру действия --> ")
     while (int(action) < 1 or int(action) > 6):
         action = input("ВЫБРАН НЕ ВЕРНЫЙ ПУНКТ !!!\nВведите верный пункт меню ")
     match action:
         case '1':
-            print(1)
+            Create()
         case '2':
-            print(2)
+            Find()
         case '3':
             print(3)
         case '4':
             print(4)
         case '5':
-            print(5)
+            Del_note()
         case '6':
-            print(6)
+            exit()
+
+
+def Second_screen():
+    print("\n" * 100)
+    action = input(
+        "Продолжаем работать в приложении!\nВыберете действие:\n1 - Создать записку\n2 - Найти записку"
+        "\n3 - Просмотреть все записки\n4 - Редактировать записку\n5 - Удалить записку\n6 - Выход из программы\nВведите "
+        "цифру действия --> ")
+    while (int(action) < 1 or int(action) > 6):
+        action = input("ВЫБРАН НЕ ВЕРНЫЙ ПУНКТ !!!\nВведите верный пункт меню ")
+    match action:
+        case '1':
+            Create()
+        case '2':
+            Find()
+        case '3':
+            print(3)
+        case '4':
+            print(4)
+        case '5':
+            Del_note()
+        case '6':
+            exit()
+
+def Create():
+    print("\n" * 100)
+    today = datetime.datetime.today()
+    today = today.strftime("%m/%d/%Y")
+    name = input('Введите название записи и нажмите "Enter"\n')
+    text = input('Напишите техт заметки и нажмите "Enter"\n')
+    controller.Create_note(name, text, today)
+    Second_screen()
+
+
+def Find():
+    print("\n" * 100)
+    action = input(
+        "Выберете метод поиска:\n1 - по номеру:\n2 - по названию:\n3 - по содержимому:"
+        "\n4 - по дате:\n5 - выход в главное меню:\n6 - Выход из программы\nВведите цифру действия --> ")
+    while (int(action) < 1 or int(action) > 6):
+        action = input("ВЫБРАН НЕ ВЕРНЫЙ ПУНКТ !!!\nВведите верный пункт меню ")
+    match action:
+        case '1':
+            print("\n" * 100)
+            action = input("Введите номер записи -->")
+            note = controller.Find_numb(action)
+            key = list(note.keys())
+            result = []
+            for keys, val in note.items():
+                result.append(val[0])
+                result.append(val[1])
+                result.append(val[2])
+                if len(val) > 3:
+                    result.append(val[3])
+            if len(result) == 0:
+                print("Записи с таким номер не существует")
+            else:
+                print(f"Запись № {key[0]}\nНазвание{result[0]}\n{result[1]}\nДата создания{result[2]}")
+            if len(note.values()) == 4:
+                print(f"Дата последнего изменения {result[3]}")
+            Second_screen()
+        case '2':
+            print("\n" * 100)
+            action = input("Введите название записи -->")
+            note = controller.Find_name(action)
+            key = list(note.keys())
+            result = []
+            for keys, val in note.items():
+                result.append(val[0])
+                result.append(val[1])
+                result.append(val[2])
+                if len(val) > 3:
+                    result.append(val[3])
+            if len(result) == 0:
+                print("Записи с таким номер не существует")
+            else:
+                print(f"Запись № {key[0]}\nНазвание{result[0]}\n{result[1]}\nДата создания{result[2]}")
+            if len(note.values()) == 4:
+                print(f"Дата последнего изменения {result[3]}")
+            Second_screen()
+        case '3':
+            print("\n" * 100)
+            action = input("Введите название записи -->")
+            note = controller.Find_text(action)
+            key = list(note.keys())
+            result = []
+            for keys, val in note.items():
+                result.append(val[0])
+                result.append(val[1])
+                result.append(val[2])
+                if len(val) > 3:
+                    result.append(val[3])
+            if len(result) == 0:
+                print("Записи с таким номер не существует")
+            else:
+                print(f"Запись № {key[0]}\nНазвание{result[0]}\n{result[1]}\nДата создания{result[2]}")
+            if len(note.values()) == 4:
+                print(f"Дата последнего изменения {result[3]}")
+            Second_screen()
+        case '4':
+            print("\n" * 100)
+            Second_screen()
+        case '5':
+            print("\n" * 100)
+            Second_screen()
+        case '6':
+            exit()
+
+
+def Del_note():
+    print("\n" * 100)
+    action = input("Введите номер записи которую нужно удалить -->")
+    controller.Del_note(action)
+    Second_screen()
